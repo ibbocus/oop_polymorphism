15fa0a85e8a5636a49c6f38aa5b5dc7eaded88b6
ibbocus <ib_bocus@hotmail.com>
2020-07-04 14:25:59 +0100

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+# The Problem
+
+# "Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+
+# NOTE -> Must be in class and method format
+
+class Fizzbuzz:
+
+    def fizzbuzz(self):
+        for i in range(1, 101):
+            if (i % 5) == 0 and (i % 3) == 0:
+                print("fizzbuzz")
+            elif i % 3 == 0:
+                print("fizz")
+            elif i % 5 == 0:
+                print("buzz")
+            else:
+                print(i)
